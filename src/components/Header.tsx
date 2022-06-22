eac70c66ecf18b5d5167d56aa523228ed44360a8
matsuboshi <matsuboshi@hotmail.com>
2022-06-21 23:18:20 -0300

    feat: building header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,9 @@
+import { Logo } from './Logo'
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <header className='w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600'>
+      <Logo />
+    </header>
+  )
 }
