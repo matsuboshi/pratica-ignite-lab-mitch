b5b05c921c1cb9cbff06d2d62b86973630da5444
matsuboshi <matsuboshi@hotmail.com>
2022-06-22 01:17:23 -0300

    feat: new api

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master',
+    // uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4od73941vhh01z7c7ip8afu/master',
     cache: new InMemoryCache()
 })
