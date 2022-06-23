ff574158cbd863192b8871464353883910af7de2
matsuboshi <matsuboshi@hotmail.com>
2022-06-23 01:43:12 -0300

    feat: fetching info according to props.slug

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,4 +1,5 @@
 import { DefaultUi, Player, Youtube } from '@vime/react'
+import { gql, useQuery } from '@apollo/client'
 import {
   CaretRight,
   DiscordLogo,
@@ -8,13 +9,62 @@ import {
 
 import '@vime/core/themes/default.css'
 
-export default function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string
+    videoId: string
+    description: string
+    teacher: {
+      bio: string
+      avatarURL: string
+      name: string
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string
+}
+
+export default function Video(props: VideoProps) {
+  console.log(props)
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  console.log('###', props.lessonSlug, data)
+
+  if (!data) {
+    return (
+      <div className='flex-1'>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
   return (
     <div className='flex-1'>
       <div className='bg-black flex justify-center'>
         <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
           <Player>
-            <Youtube videoId='SO4-izct7Mc' />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -23,26 +73,24 @@ export default function Video() {
       <div className='p-8 max-w-[1100px] mx-auto'>
         <div className='flex items-start gap-16'>
           <div className='flex-1'>
-            <h1 className='text-2xl font-bold'>Abertura do Ignite Lab</h1>
+            <h1 className='text-2xl font-bold'>{data.lesson.title}</h1>
             <p className='mt-4 text-gray-200 leading-relaxed'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-              voluptas dignissimos voluptatum incidunt saepe at eligendi sunt
-              tempora ratione debitis.
+              {data.lesson.description}
             </p>
 
             <div className='flex items-center gap-4 mt-6'>
               <img
                 className='h-16 w-16 rounded-full border-2 border-blue-500'
-                src='https://github.com/matsuboshi.png'
+                src={data.lesson.teacher.avatarURL}
                 alt='matsuboshi'
               />
 
               <div className='leading-relaxed'>
                 <strong className='font-bold text-2xl block'>
-                  Mitsuhiko Takushi
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className='text-gray-200 text-sm block'>
-                  Fullstack Developer @ Stone
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
